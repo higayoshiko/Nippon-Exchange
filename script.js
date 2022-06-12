370b0a3c9060dd09461edea7374de92866cf7dbd
higayoshiko <higayoshiko@yahoo.com>
2022-06-12 22:14:37 +0900

    add api call & update table

--- script.js
+++ script.js
@@ -1,65 +1,41 @@
-
 (function() {
   const dropdown = document.querySelector(".converter-dropdown");
-  const amount = document.querySelector(".converter-input");
   const convertButton = document.querySelector(".convert-btn");
-  // const resultText = document.querySelector(".result-num");
   const burger = document.querySelector(".nav-burger");
   const hiddenNav = document.querySelector(".hidden-nav");
   const hiddenIcon = document.querySelector(".hidden-nav-item");
   const copyrightYear = document.querySelector(".copyrightYear");
+  const resultValue = document.querySelector(".results-val");
+  const resultAcronym = document.querySelector(".results-acronym--1");
+  const resultAcronymTwo = document.querySelector(".results-acronym--sub");
+  const currentRateInCoverter = document.querySelector(".results-rate");
+  const tableCurrencies = document.querySelectorAll(".currencies span");
   const rateUpdateDateAndTime = document.querySelector(".heading-sub-ss span");
-  const currentRateInCoverter = document.querySelector(".result-rate");
-  const resultValue = document.querySelector(".result-val");
-  const resultAcronym = document.querySelector(".result-acronym");
-
-
+  let rateList = {};
   let acronym = "";
 
   copyrightYear.innerHTML = new Date().getFullYear();
 
-  let rateList = {};
+  tableCurrencies.forEach(function(currency) {
 
-  function storeJsonAsObjects(buyJson, sellJson) {
-
-      if(!rateList[buyJson["base_code"]] && !rateList[sellJson["target_code"]]){
-        rateList[buyJson["base_code"]] = {
-          "BUY": buyJson["conversion_rate"],
-          "SELL": sellJson["conversion_rate"]
-        }
-    }
-  };
-
-  const tableCurrencies = document.querySelectorAll(".currencies span");
-
-  tableCurrencies.forEach(async function(currency) {
-
-    const apiKeys = "";
     let currencyNames = currency.innerHTML.toUpperCase();
-    let selectSellCell = currency.parentNode.nextElementSibling;
-    let selectBuyCell = selectSellCell.nextElementSibling;
+    let selectBuyCell = currency.parentNode.nextElementSibling;
+    let buyJson;
 
     try {
-      let [buyFetch, sellFetch] = await Promise.all([
-        //buy rates
-        fetch(`https://v6.exchangerate-api.com/v6/${apiKeys}/pair/${currencyNames}/JPY`),
-        //sell rates
-        fetch(`https://v6.exchangerate-api.com/v6/${apiKeys}/pair/JPY/${currencyNames}`)
-      ]);
-
-      (async function convertToJson() {
-        let buyJson = await buyFetch.json();
-        let sellJson = await sellFetch.json();
-        storeJsonAsObjects(buyJson, sellJson);
-
-        if(currencyNames === buyJson["base_code"] && currencyNames === sellJson["target_code"]){
-          selectSellCell.innerHTML = sellJson["conversion_rate"];
-          selectBuyCell.innerHTML = buyJson["conversion_rate"];
-        }
-
-        rateUpdateDateAndTime.innerHTML = buyJson["time_last_update_utc"];
-
-      })();
+      //JPY base currency to other currencies
+        fetch(`https://open.er-api.com/v6/latest/JPY`)
+        .then(async response => {
+          buyJson = await response.json();
+        })
+        .then(() => {
+          rateUpdateDateAndTime.innerHTML = buyJson["time_last_update_utc"];
+          selectBuyCell.innerHTML = buyJson.rates[currencyNames];
+          // storeJsonAsObjects(buyJson.rates);
+        })
+        .then(() => {
+          rateList[currencyNames] = buyJson.rates[currencyNames];
+        });
     } catch (err) {
       console.log(err)
     }
@@ -68,10 +44,10 @@
   //show conversion result
   function showResult(value, result) {
     // resultText.innerHTML = `${value} JPY = ${result} ${acronym}`;
-    resultValue.innerHTML = value;
-    currentRateInCoverter.innerHTML = result;
+    const resultAmount = document.querySelector(".results-amount");
+    resultValue.innerHTML = result;
+    resultAmount.innerHTML = value;
     resultAcronym.innerHTML = acronym;
-
   }
 
   //function to change text inside the default option box
@@ -79,23 +55,23 @@
     const links = list.querySelectorAll(".dropdown-link");
     let span = currentCurrency.querySelector(".default-currency");
 
-
     links.forEach(item => {
       item.addEventListener("click", function() {
         acronym = this.querySelector("span").innerHTML;
         let newCurrency = this.innerHTML;
         span.innerHTML = newCurrency;
-        currentRateInCoverter.innerHTML = rateList[acronym].BUY;
-        resultValue.innerHTML = "1";
+        currentRateInCoverter.innerHTML = rateList[acronym];
+        resultAcronymTwo.innerHTML = acronym;
       });
     });
   };
 
   convertButton.addEventListener("click", function() {
+    const amount = document.querySelector(".converter-input");
     //get amount/value and convert it to a number
     value = Number(amount.value);
-    result = (value / rateList[acronym].BUY).toFixed(2);
-     showResult(value, result);
+    result = (value / rateList[acronym]).toFixed(2);
+    showResult(value, result);
   });
 
   //select dropdown menu and apply eventListener
